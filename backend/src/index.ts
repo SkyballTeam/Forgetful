97221a2b9d15115efc7d3e5cd3f2417d61ab3583
Ayush Saini <gamerzayush62@gmail.com>
2026-04-06 17:19:27 +0530

    Configure frontend and backend env vars for separate publishing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*'
+}));
 app.use(bodyParser.json());
 
 async function startServer() {
